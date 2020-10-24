46b7eae3f618c0846d4d94ed306d4d124d042d92
DESKTOP-LQLTC1T\flavi <fcastrofh@gmail.com>
2020-10-23 21:59:03 -0300

    Add departament

---
+++ src/main/java/com/project/professor/allocation/model/Departament.java
@@ -0,0 +1,34 @@
+package com.project.professor.allocation.model;
+
+public class Departament {
+
+	private long id;
+	private String name;
+
+	public Departament() {
+		super();
+	}
+
+	public Departament(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
