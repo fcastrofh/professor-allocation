0dde6fa6a4bb0504266c1cdeec17013f9a0192af
DESKTOP-LQLTC1T\flavi <fcastrofh@gmail.com>
2020-10-23 22:00:14 -0300

    Add course

---
+++ src/main/java/com/project/professor/allocation/model/Course.java
@@ -0,0 +1,34 @@
+package com.project.professor.allocation.model;
+
+public class Course {
+
+	private long id;
+	private String name;
+
+	public Course() {
+		super();
+	}
+
+	public Course(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
