6f82e6f5eb0545c16976fe1a630dae670d8051e0
DESKTOP-LQLTC1T\flavi <fcastrofh@gmail.com>
2020-10-28 22:07:08 -0300

    Add test professor readAll

--- src/test/java/com/project/professor/allocation/repository/ProfessorRepositoryTes.java
+++ src/test/java/com/project/professor/allocation/repository/ProfessorRepositoryTes.java
@@ -1,5 +1,33 @@
 package com.project.professor.allocation.repository;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import com.project.professor.allocation.model.Professor;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
 public class ProfessorRepositoryTes {
+	
+	@Autowired
+	private ProfessorRepository professorRepository;
+	
+	@Test
+	public void findAll() {
+		// Act
+		List<Professor> professors = professorRepository.findAll();
+
+		// Print
+		professors.stream().forEach(System.out::println);
+	}
 
 }
